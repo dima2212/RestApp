4f7464ffe57a06c1c7cd02f7c765a324c7873d34
Dumitru Moraru <d.moraru2212@gmail.com>
2019-08-28 15:22:45 +0300

    2nd commit

--- src/main/java/com/example/RestFileApi/RestApplication.java
+++ src/main/java/com/example/RestFileApi/RestApplication.java
@@ -9,5 +9,4 @@ public class RestFileApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestFileApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/RestFileApi/RunnerClass.java
+++ src/main/java/com/example/RestFileApi/RunnerClass.java
@@ -1,6 +1,8 @@
 package com.example.RestFileApi;
 
 import com.example.RestFileApi.service.FileImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,11 @@ public class Controller {
     @Value("${files.path}")
     private String destination;
 
+    private final static Logger log = LoggerFactory.getLogger(FileImpl.class);
+
     @RequestMapping("/files/{fileId}")
     public ResponseEntity<Object> getFile(@PathVariable("fileId")int fileId) {
+        log.info("File name is :" + fileId);
         FileImpl file = new FileImpl();
         ResponseEntity<Object> response = file.downloadFile(fileId, destination);
         return response;
